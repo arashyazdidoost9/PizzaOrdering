a1d4afd7a952afcd7da073d4d765deb5cc875e76
Arash Yazdidoost <47757609+arashyazdidoost9@users.noreply.github.com>
2020-12-02 12:54:59 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config.json");
+
+const express = require("express");
+const { join } = require("path");
+const app = express();
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ["RS256"]
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your order has been placed and your pizza will be on its way soon!!"
+  });
+});
+
+// Serve static assets from the /public folder
+app.use(express.static(join(__dirname, "public")));
+
+// Endpoint to serve the configuration file
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+// Serve the index page for all other requests
+app.get("/*", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Application running on port 3000"));
